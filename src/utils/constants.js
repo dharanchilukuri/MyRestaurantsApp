64136f1ba8441613dc90ddb4dff18802d119f0a0
dharanchilukuri <dharanchilukuri@gmail.com>
2024-09-29 14:44:11 +0530

    3

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,6 +4,7 @@ export const CDN_URL ="https://media-assets.swiggy.com/swiggy/image/upload/fl_lo
 
 export const LOGO_URL ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzRUpf7CjL4QJZF4-P9wMoTiWaF2IDEI5Aww&s";
 
+//other way
 // export const API_URL = process.env.REACT_APP_SWIGGY_API;
 // export const CDN_URL = process.env.REACT_APP_CLOUDINARY_URL;
 // export const LOGO_URL = process.env.REACT_APP_LOGO_URL;
