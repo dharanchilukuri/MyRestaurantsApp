5907423e6b9d659ce0f0a66d693c6fdf11bd89d1
dharanchilukuri <dharanchilukuri@gmail.com>
2024-09-29 10:19:19 +0530

    2

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,4 @@
-export const API_URL = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.7651566&lng=83.356231&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+export const API_URL = process.env.REACT_APP_SWIGGY_API; 
+export const CDN_URL = process.env.REACT_APP_CLOUDINARY_URL; 
 
-export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
-
-export const LOGO_URL = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzRUpf7CjL4QJZF4-P9wMoTiWaF2IDEI5Aww&s";
-
+export const LOGO_URL = process.env.REACT_APP_LOGO_URL; 
