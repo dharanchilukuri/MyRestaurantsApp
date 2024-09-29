f76cb583a184d449fb87eca7be239c8fda4a2a22
dharanchilukuri <dharanchilukuri@gmail.com>
2024-09-29 10:41:09 +0530

    3

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const API_URL = process.env.REACT_APP_SWIGGY_API; 
-export const CDN_URL = process.env.REACT_APP_CLOUDINARY_URL; 
+export const API_URL = process.env.REACT_APP_SWIGGY_API;
+export const CDN_URL = process.env.REACT_APP_CLOUDINARY_URL;
+export const LOGO_URL = process.env.REACT_APP_LOGO_URL;
 
-export const LOGO_URL = process.env.REACT_APP_LOGO_URL; 
